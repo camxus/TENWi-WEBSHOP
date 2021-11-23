4ed41b0677e3ab53dc92edfef2e77ab969f6b0c8
Camillus Konkwo <camillus.konkwo@gmail.com>
2021-11-23 10:18:40 +0000

    Add files via upload

---
+++ src/queries/product-and-categories.js
@@ -0,0 +1,91 @@
+import { gql } from "@apollo/client";
+
+/**
+ * GraphQL categories and products query.
+ */
+const PRODUCTS_AND_CATEGORIES_QUERY = gql`query {
+  heroCarousel: productCategories(where: {slug: "offers"}) {
+    nodes {
+      id
+      children {
+        nodes {
+          id
+          name
+          slug
+          databaseId
+          description
+          image {
+            id
+            sourceUrl
+            srcSet
+          }
+        }
+      }
+    }
+  }
+  productCategories(first: 50) {
+    nodes {
+      id
+      name
+      slug
+      image {
+        id
+        sourceUrl
+        srcSet
+      }
+    }
+  }
+  productTags(first: 50) {
+    nodes {
+      id
+      name
+      slug
+    }
+  }
+  products(first: 50) {
+    nodes {
+      id
+      productId: databaseId
+      averageRating
+      slug
+      description
+      image {
+        id
+        altText
+        sourceUrl
+      }
+      name
+      ... on SimpleProduct {
+        price
+        regularPrice
+        id
+      }
+      ... on VariableProduct {
+        price
+        id
+        regularPrice
+      }
+      ... on ExternalProduct {
+        price
+        id
+        externalUrl
+        regularPrice
+      }
+      ... on GroupProduct {
+        id
+        products {
+          nodes {
+            ... on SimpleProduct {
+              id
+              price
+              regularPrice
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
+export default PRODUCTS_AND_CATEGORIES_QUERY;
