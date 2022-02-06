6526627337697228f5a0d616102e619cc1661d81
cam <camillus.konkwo@gmail.com>
2022-02-06 11:03:20 +0100

    checkout

--- src/components/checkout/Paypal.js
+++ src/components/checkout/Paypal.js
@@ -39,7 +39,7 @@ export default function Paypal({cart, input, products, setRequestError, clearCar
     if (window.paypal?.Buttons === undefined){
         // window.location.replace("/checkout");
         // paypal = useRef()
-        useEffect(() =>  {setIsLoaded(false)},[isLoaded])
+        setIsLoaded(false)
     }
     return (
         <div>
