361d8432b2deb3847dc9be225c3f5269e8810a89
cam <camillus.konkwo@gmail.com>
2022-02-15 17:09:27 +0100

    newsletter

--- src/components/NewsletterSubmit.js
+++ src/components/NewsletterSubmit.js
@@ -3,13 +3,15 @@ import {useMutation, useQuery} from '@apollo/client';
 import NEWSLETTER_MUTATION from "../mutations/newsletter.js";
 import {useState} from "react"
 import styles from "../styles/newsletter.module.css"
+import Mailchimp from 'mailchimp-api-v3'
+
 
 
 const NewsletterSubmit = () => {
 	const [email, setEmail] = useState("");
 	const [errorHandler, setErrorHandler] = useState("");
 	const [requestError, setRequestError] = useState(null);
-
+	var mailchimp = Mailchimp(process.env.MAILCHIMP_API_KEY);
 
 	const handler = (event) => {
         const value = event.target.value
@@ -44,9 +46,23 @@ const NewsletterSubmit = () => {
         }
     });
 
+	function addUserToMailchimp(email){
+		mailchimp.post('/lists/id/members', {
+			email_address : email,
+			status : 'subscribed'
+		  })
+		  .then(function(results) {
+			setErrorHandler("done")
+		  })
+		  .catch(function (err) {
+			setErrorHandler("error")
+            console.log("ERROR ", err)
+		  })
+	}
 	const register = (event) => {
 		event.preventDefault();
-		registerMutation()
+		addUserToMailchimp(email)
+		// registerMutation()
 		// console.log("completed with", email)
 	}
 
