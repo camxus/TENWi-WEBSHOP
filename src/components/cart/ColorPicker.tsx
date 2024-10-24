1bcdace53f06f16ace4e6e1fb2f1774a4c302753
Camillus Konkwo <camillus.konkwo@gmail.com>
2024-10-24 13:13:36 +0200

    auto select color

--- src/components/cart/ColorPicker.tsx
+++ src/components/cart/ColorPicker.tsx
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function ColorPicker({ options, selectedOptions, onClick }: any) {
+  const [colorOptions] = options.filter(
+    (option: { name: any; options: string[] }) =>
+      option.name.toLowerCase() === "color"
+  );
+
+  useEffect(() => {
+    if (colorOptions && colorOptions.options?.length === 1) {
+      onClick({
+        label: colorOptions.options[0],
+        value: colorOptions.name,
+      });
+    }
+  }, [colorOptions]);
+
   return (
     <>
-      {options.length &&
-        options
-          .filter(
-            (option: { name: any; options: string[] }) =>
-              option.name.toLowerCase() === "color"
-          )
-          .map((option: { name: any; options: any[] }) => (
-            <div key={option.name} className="flex justify-end p-2 px-4 gap-1">
-              {option.options.map((value: any) => (
-                <div
-                  key={value}
-                  onClick={() =>
-                    onClick({
-                      label: value,
-                      value: option.name,
-                    })
-                  }
-                  className="rounded-full w-4 h-4 border-black border-2 cursor-pointer"
-                  style={{
-                    backgroundColor: value,
-                    opacity: selectedOptions["color"] === value ? 1 : 0.5,
-                  }}
-                ></div>
-              ))}
-            </div>
+      {colorOptions && (
+        <div
+          key={colorOptions.name}
+          className="flex justify-end p-2 px-4 gap-1"
+        >
+          {colorOptions.options.map((value: any) => (
+            <div
+              key={value}
+              onClick={() =>
+                onClick({
+                  label: value,
+                  value: colorOptions.name,
+                })
+              }
+              className="rounded-full w-4 h-4 border-black border-2 cursor-pointer"
+              style={{
+                backgroundColor: value,
+                opacity:
+                  selectedOptions["color"] === value ||
+                  selectedOptions["COLOR"] === value
+                    ? 1
+                    : 0.5,
+              }}
+            ></div>
           ))}
+        </div>
+      )}
     </>
   );
 }
